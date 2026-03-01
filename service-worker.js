53b57ec52056ba51277cc803d6c30e6b64e1f10a
ehabamr062-ux <ehabamr062@gmail.com>
2026-03-01 16:49:47 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "noor-app-cache-v1";
+const urlsToCache = [
+    "index.html",
+    "icon.png",
+    "manifest.json"
+];
+
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
